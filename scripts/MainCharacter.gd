015759bc8e1c833d2560d4b742ec93bab3790a1d
ChaseRay <chaserayemail@gmail.com>
2024-07-30 20:00:32 -0500

    josh's formatted edits

--- scripts/MainCharacter.gd
+++ scripts/MainCharacter.gd
@@ -1,15 +1,11 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -325.0
+const SPEED = 200.0
+const JUMP_VELOCITY = -300.0
 var count = 0
-var push_force = 20.0
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
-@onready var jump = $Jump
-@onready var walk = $Walk
-
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -26,24 +22,14 @@ func _physics_process(delta):
 	else:
 		if (velocity.x > 1 || velocity.x < -1):
 			animated_sprite_2d.animation = "walk"
-			if !walk.playing:
-				walk.play()
 		else:
 			animated_sprite_2d.animation = "idle"
-			walk.stop()
 			
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		animated_sprite_2d.animation = "jump"
-		if walk.playing:
-				walk.stop()
-		jump.play()
 		velocity.y = JUMP_VELOCITY
 		count = 0
-	for i in get_slide_collision_count():
-		var c = get_slide_collision(i)
-		if c.get_collider() is RigidBody2D:
-			c.get_collider().apply_central_impulse(-c.get_normal() * push_force)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -62,6 +48,4 @@ func _physics_process(delta):
 		animated_sprite_2d.flip_h = false
 
 	move_and_slide()
-	
-
 	
