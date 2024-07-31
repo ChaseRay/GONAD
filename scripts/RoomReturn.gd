b2a8514db1e0864b9c357ab0a1d4c6fcf15c9714
ChaseRay <chaserayemail@gmail.com>
2024-07-30 20:20:59 -0500

    pls fix

--- scripts/RoomReturn.gd
+++ scripts/RoomReturn.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 var space = false
 @onready var animation_player = $PortalTeleport/AnimationPlayer
-
+@onready var portal = $Portal
 
 func _on_body_entered(_body: PhysicsBody2D):
 	space = true # Replace with function body.
@@ -15,5 +15,6 @@ func _physics_process(_delta):
 	if space == true:
 		if Input.is_action_just_pressed("ui_select"):
 			animation_player.play("teleport")
-			await get_tree().create_timer(0.5).timeout
+			portal.play()
+			await get_tree().create_timer(0.7).timeout
 			get_tree().change_scene_to_file("res://scenes/MainRoomScene.tscn")
