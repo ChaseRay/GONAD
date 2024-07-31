f48fccdb04886e015d1a6a453276e10927fc997c
HogHogHogHogHog <bobertoe66@gmail.com>
2024-07-30 22:53:37 -0500

    boob

--- main menu.gd
+++ main menu.gd
@@ -3,10 +3,9 @@ extends Control
 
 
 
-func _on_button_pressed():
+func _on_button_2_pressed():
 	get_tree().change_scene_to_file("res://scenes/MainRoomScene.tscn")
 
 
-
-func _on_button_2_pressed():
+func _on_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/credits.tscn")
